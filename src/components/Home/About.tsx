fe1381946017694e1d7dc5d3d8d799cf3da126be
Massimo-Romairone <mopvladd@gmail.com>
2026-01-27 23:57:07 -0300

    Pequeñas modificaciones

--- src/components/Home/About.tsx
+++ src/components/Home/About.tsx
@@ -6,8 +6,7 @@ const About = () => {
         <div className="aboutSection1">
           <p>About me</p>
             <p className="pAboutMe">
-              ¡Hola! Soy Massimo Gianfranco Romairone, Desarrollador Web Fullstack con una clara especialización en backend.
-              Me encanta transformar ideas en código y desarrollar soluciones backend sólidas, utilizando herramientas como NestJS y diferentes sistemas de bases de datos.
+              ¡Hola! Soy Massimo Gianfranco Romairone, Desarrollador Web Full Stack con una clara especialización en backend, utilizando herramientas como NestJS y diferentes sistemas de bases de datos.<br />
               Sigo aprendiendo cada día, probando nuevas tecnologías y mejorando mis habilidades para enfrentar desafíos cada vez más complejos.<br />
               Backend sólido, validaciones al día y seguridad siempre como prioridad.
             </p>
